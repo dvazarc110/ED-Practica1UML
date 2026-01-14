451a8c7fbe0a107328eecbf8d0c59b013d6270aa
dvazarc110 <dvazarc110@g.educaand.es>
2026-01-14 11:13:21 +0100

    14/01/2026 "Mañana"

--- ED-Practica1UML/src/prueba/Pedido.java
+++ ED-Practica1UML/src/prueba/Pedido.java
@@ -8,36 +8,52 @@ public class Pedido {
 	public int numero;
 	private EstadoPedido estado;
 	private List<Producto> productos;
+	private String resume = "";
 	
-	 public Pedido(int numero) {
-	        this.numero = numero;
-	        this.estado = EstadoPedido.PENDIENTE; // estado inicial lógico
-	        this.productos = new ArrayList<>();
-	    }
+	public Pedido(int numero) {
+		this.numero = numero;
+	    this.estado = EstadoPedido.PENDIENTE; // estado inicial lógico
+	    this.productos = new ArrayList<>();
+	}
 
-	    public void agregarProducto(Producto p) {
-	        productos.add(p);
-	    }
+	public void agregarProducto(Producto p) {
+	    productos.add(p);
+	}
 
-	    public double calcularTotal() {
-	        double total = 0.0;
-	        for (Producto p : productos) {
-	            total += p.getPrecio();
-	        }
-	        return total;
+	public double calcularTotal() {
+	    double total = 0.0;
+	    for (Producto p : productos) {
+	    	total += p.getPrecio();
 	    }
+	    return total;
+	}
+	
+	public String resumePedido() {
+		String resume = "";
+	    for (Producto p : productos) {
+	    	resume = resume.concat(String.format("%s, ",p.getNombre()));
+	    }
+	    resume = resume.substring(0, (resume.length()-2));
+	    return resume;
+	}
 
-	    public int getNumero() {
-	        return numero;
-	    }
+	public int getNumero() {
+	    return numero;
+	}
 
-	    public EstadoPedido getEstado() {
-	        return estado;
-	    }
+	public EstadoPedido getEstado() {
+	    return estado;
+	}
 
-	    public void setEstado(EstadoPedido estado) {
-	        this.estado = estado;
-	    }
+	public void setEstado(EstadoPedido estado) {
+	    this.estado = estado;
+	}
 	
 
+	@Override
+	public String toString() {
+		return String.format("Pedido: %d \nProductos: %s \nEstado: %s \nTotal: %.2f \n", numero, resumePedido(), estado, calcularTotal()); 
+	}  
+	
+	
 }
--- ED-Practica1UML/src/prueba/main.java
+++ ED-Practica1UML/src/prueba/main.java
@@ -11,6 +11,7 @@ public class main {
 		Pedido pedido;
 		Producto producto;
 		Scanner keyboard = new Scanner(System.in);
+		boolean next = false;
 		int cantidad;
 		int cont = 0;
 		pedido = new Pedido(1);
@@ -18,12 +19,31 @@ public class main {
 		cantidad = keyboard.nextInt();
 		
 		while (cont!=cantidad) {
-			System.out.printf("\nNombre del producto número %d", cont+1);
-			String nombre = keyboard.nextLine();
-			System.out.printf("\nPrecio del producto número %d", cont+1);
+			System.out.printf("\nNombre del producto número %d\n", cont+1);
+			String nombre = keyboard.next();
+			
+			System.out.printf("Precio del producto número %d\n", cont+1);
 			double precio = keyboard.nextDouble();
 			producto = new Producto(nombre, precio);
+			pedido.agregarProducto(producto);
+			cont++;
+			if(cont == cantidad) {
+				System.out.printf("\nDetalles: \n%s", pedido);
+				while(next==false) {
+					System.out.println("¿Quiere enviar su pedido?(Y/N)");
+					String respuesta = keyboard.next();
+					if((respuesta.isEmpty()) || ((respuesta.toLowerCase()).charAt(0) != 'y')) {
+						System.out.println("Porfavor, envíe su pedido.");
+						next = false;
+					}else {
+						next = true;
+						pedido.setEstado(EstadoPedido.ENVIADO);
+						System.out.printf("\nEstupendo, su pedido ha sido enviado. \nDetalles: \n%s", pedido);
+					}
+				}
+			}
 		}
+		
 	}
 	
 	public static void main(String[] args) {
